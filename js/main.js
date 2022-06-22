4928964f46ec9e606e457237bd1328aacf898af4
Mario Flota <mflota@gmail.com>
2022-06-21 23:35:32 -0600

    Week08

--- js/main.js
+++ js/main.js
@@ -27,6 +27,10 @@ const links = [
     {
         label: "Week 7 notes",
         url: "week07/index.html"
+    },
+    {
+        label: "Week 8 notes",
+        url: "week08/index.html"
     }
 ]
 
